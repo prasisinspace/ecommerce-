ac0a1439a0ea45d2d985e5834c00bd1cb36c37b8
prasisinspace <107873954+prasisinspace@users.noreply.github.com>
2023-06-23 23:04:40 +0545

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from'morgan';
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js';
+import cors from 'cors';
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+
+const app = express();
+
+//midlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+//routes
+app.use('/api/v1/auth',authRoutes);
+
+
+//rest api
+app.get('/',(req,res)=>{
+    res.send("<h1>welcome to app</h1>");
+})
+
+//PORT
+const PORT= process.env.PORT || 3001;
+
+//run listen
+app.listen(PORT, ()=>{
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
